ddf63c014e5935cb4259b7917c31d80234b7b3dd
maryamnawas <maryamnawas2002@gmail.com>
2024-06-07 10:14:08 +0530

    logo done

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div  className='bg-white dark:bg-gray-900 dark:text-white'>
+        <div className='py-4'>
+            <div className='container'>
+                {/* logo and link section*/}
+                <div>
+                    <a href='#' className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>Eshop</a>
+
+                    {/* Menu Items */}
+
+                </div>
+
+                {/* Navbar section*/}
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
